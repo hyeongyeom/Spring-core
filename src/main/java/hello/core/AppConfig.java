2d862d6a09597abeedb04f94a718edf9540aae00
hyeongyeom <wltnwkd244@naver.com>
2022-07-20 18:23:52 +0900

    set Dependency Injection AppConfig

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+
+    private MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    private RateDiscountPolicy DiscountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -10,8 +10,10 @@ import static hello.core.member.Grade.VIP;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig=new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId=1L;
         Member member=new Member(memberId,"memberA",Grade.VIP);
